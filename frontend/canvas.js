a2c3ed1b2bfa6ba335c2721c61eb03631a68a92a
PES2UG24CS818 <pes2202401442@pesu.pes.edu>
2026-04-04 11:52:23 +0530

    feat: day1 frontend canvas + docker-compose + README

---
+++ frontend/canvas.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById("board");
+const ctx = canvas.getContext("2d");
+
+let drawing = false;
+
+canvas.addEventListener("mousedown", () => drawing = true);
+
+canvas.addEventListener("mouseup", () => {
+	drawing = false;
+	ctx.beginPath();
+});
+
+canvas.addEventListener("mouseleave", () => {
+	drawing = false;
+	ctx.beginPath();
+});
+
+canvas.addEventListener("mousemove", draw);
+
+function draw(e) {
+	if (!drawing) return;
+
+	ctx.lineWidth = 2;
+	ctx.lineCap = "round";
+
+	ctx.lineTo(e.offsetX, e.offsetY);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(e.offsetX, e.offsetY);
+}
